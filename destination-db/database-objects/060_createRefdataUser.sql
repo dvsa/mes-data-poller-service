f652fb11f71dc856bb09863d0b332f38a3763e6a
hiddlestonej <john.hiddlestone@dvsa.gov.uk>
2023-04-05 10:50:43 +0100

    Seperated out create refdata user so part of user creation process with DMS User

--- destination-db/database-objects/060_createRefdataUser.sql
+++
@@ -1,4 +0,0 @@
-USE tarsreplica;
-CREATE USER IF NOT EXISTS 'refdata' IDENTIFIED WITH AWSAuthenticationPlugin as 'RDS';
-GRANT SELECT ON tarsreplica.TEST_CENTRE TO 'refdata';
-GRANT SELECT ON tarsreplica.TEST_CENTRE_NAME TO 'refdata';
