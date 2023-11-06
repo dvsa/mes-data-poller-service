657605d8918c396d4af879259ae65dc7553b1586
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2023-11-06 09:41:55 +0000

    removed un-used files

--- src/functions/pollJournals/framework/databases/mysql/__tests__/advance-test-slot-repository.int.module.ts
+++
@@ -1,8 +0,0 @@
-export const advanceTestSlotsIntegrationTests = () => {
-  describe('AdvanceTestSlotRepository', () => {
-
-    it('should run advance test slot integration tests', async () => {
-      // Placeholder to prove grouping of integration test suites
-    });
-  });
-};
