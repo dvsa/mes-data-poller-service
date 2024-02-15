abc121bde4e7002090caa8350d33153a4d147e60
Matthew Bell <mbell.dvsa@icloud.com>
2024-02-15 09:02:08 +0000

    Revert unused import

--- src/functions/pollUsers/domain/transfer-users.ts
+++ src/functions/pollUsers/domain/transfer-users.ts
@@ -2,7 +2,6 @@ import { getActiveExaminers } from '../framework/repo/mysql/examiner-repository'
 import { getCachedExaminers } from '../framework/repo/dynamodb/cached-examiner-repository';
 import { reconcileActiveAndCachedExaminers } from './examiner-cache-reconciler';
 import { getUniversalTestPermissions } from '../framework/repo/mysql/universal-permissions-repository';
-import {StaffDetail} from '../../../common/application/models/staff-details';
 
 export const transferUsers = async () => {
   const universalTestPermissions = await getUniversalTestPermissions();
