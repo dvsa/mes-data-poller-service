e114683c9bb0866da69ce0403eea4d510a1421e4
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-01-14 14:21:29 +0000

    utilised shared des booking

--- src/functions/pollJournals/framework/databases/mysql/test-slot-respository-des-schedule.ts
+++ src/functions/pollJournals/framework/databases/mysql/test-slot-respository-des-schedule.ts
@@ -3,121 +3,10 @@ import * as moment from 'moment';
 import { info, customDurationMetric } from '@dvsa/mes-microservice-common/application/utils/logger';
 import { ExaminerTestSlot } from '../../../domain/examiner-test-slot';
 import { drizzle, MySql2Database } from 'drizzle-orm/mysql2/driver';
-import {
-  mysqlTable,
-  varchar,
-  int,
-  tinyint,
-  decimal,
-  text,
-  json,
-  datetime,
-  mysqlEnum,
-
-} from 'drizzle-orm/mysql-core';
 import { and, gte, inArray, lte } from 'drizzle-orm';
 import { ExaminerRecord } from '../../../domain/examiner-record';
 import { eq } from 'drizzle-orm/sql/expressions/conditions';
-
-export const ScheduleBookings = mysqlTable(
-  'bookings',
-  {
-    created_date: datetime('created_date').notNull(),
-    last_updated_date: datetime('last_updated_date').notNull(),
-    booking_reference: varchar('booking_reference', {length: 50}).notNull().primaryKey(),
-
-    marked_for_deletion: tinyint('marked_for_deletion'),
-    dsp_created_date: datetime('dsp_created_date').notNull(),
-    dsp_last_updated_date: datetime('dsp_last_updated_date').notNull(),
-
-    examiner_staff_number: varchar('examiner_staff_number', {length: 50}).notNull(),
-    examiner_title: varchar('examiner_title', {length: 50}).notNull(),
-    examiner_first_name: varchar('examiner_first_name', {length: 100}).notNull(),
-    examiner_second_name: varchar('examiner_second_name', {length: 100}).notNull(),
-    examiner_third_name: varchar('examiner_third_name', {length: 100}).notNull(),
-    examiner_last_name: varchar('examiner_last_name', {length: 100}).notNull(),
-
-    testslot_start: datetime('testslot_start').notNull(),
-    testslot_duration_minutes: int('testslot_duration_minutes').notNull(),
-    testslot_vehicle_type_code: mysqlEnum('testslot_vehicle_type_code', [
-      'L',
-      'C',
-      'B',
-      'T',
-      'A2',
-      'A3',
-      'V4',
-      'B1',
-      'B2',
-      'O',
-      'LM',
-      'BE',
-      'TC',
-      'SC']).notNull(),
-    testslot_vehicle_slot_type_code: int('testslot_vehicle_slot_type_code').notNull(),
-
-    testcentre_name: varchar('testcentre_name', {length: 255}).notNull(),
-    testcentre_cost_code: varchar('testcentre_cost_code', {length: 50}).notNull(),
-
-    candidate_title: varchar('candidate_title', {length: 50}).notNull(),
-    candidate_first_name: varchar('candidate_first_name', {length: 100}).notNull(),
-    candidate_second_name: varchar('candidate_second_name', {length: 100}).notNull(),
-    candidate_third_name: varchar('candidate_third_name', {length: 100}).notNull(),
-    candidate_last_name: varchar('candidate_last_name', {length: 100}).notNull(),
-    candidate_driver_number: varchar('candidate_driver_number', {length: 50}).notNull(),
-    candidate_date_of_birth: datetime('candidate_date_of_birth').notNull(),
-    candidate_gender: mysqlEnum('candidate_gender', ['M', 'F']).notNull(),
-    candidate_address_line1: varchar('candidate_address_line1', {length: 255}).notNull(),
-    candidate_address_line2: varchar('candidate_address_line2', {length: 255}).notNull(),
-    candidate_address_line3: varchar('candidate_address_line3', {length: 255}).notNull(),
-    candidate_address_line4: varchar('candidate_address_line4', {length: 255}).notNull(),
-    candidate_address_line5: varchar('candidate_address_line5', {length: 255}).notNull(),
-    candidate_postcode: varchar('candidate_postcode', {length: 20}).notNull(),
-    candidate_primary_telephone: varchar('candidate_primary_telephone', {length: 50}).notNull(),
-    candidate_secondary_telephone: varchar('candidate_secondary_telephone', {length: 50}).notNull(),
-    candidate_mobile_telephone: varchar('candidate_mobile_telephone', {length: 50}).notNull(),
-    candidate_email_address: varchar('candidate_email_address', {length: 255}).notNull(),
-    candidate_prn: int('candidate_prn'),
-    candidate_previous_adi_tests: int('candidate_previous_adi_tests'),
-    candidate_ethnicity_code: varchar('candidate_ethnicity_code', {length: 5}),
-
-    application_welsh_test: tinyint('application_welsh_test').notNull(),
-    application_extended_test: tinyint('application_extended_test').notNull(),
-    application_meeting_place: varchar('application_meeting_place', {length: 255}).notNull(),
-    application_progressive_access: tinyint('application_progressive_access').notNull(),
-    application_special_needs: text('application_special_needs').notNull(),
-    application_special_needs_ext_test: tinyint('application_special_needs_ext_test').notNull(),
-    application_special_needs_code: mysqlEnum('application_special_needs_code', ['NONE', 'YES', 'EXTRA']).notNull(),
-    application_entitlement_check: tinyint('application_entitlement_check').notNull(),
-    application_fit_marker: tinyint('application_fit_marker').notNull(),
-    application_fit_case_number: varchar('application_fit_case_number', {length: 50}).notNull(),
-    application_category_entitlement_check: tinyint('application_category_entitlement_check').notNull(),
-    application_vehicle_seats: int('application_vehicle_seats'),
-    application_vehicle_height_m: decimal('application_vehicle_height_m', {precision: 5, scale: 2}),
-    application_vehicle_width_m: decimal('application_vehicle_width_m', {precision: 5, scale: 2}),
-    application_vehicle_length_m: decimal('application_vehicle_length_m', {precision: 5, scale: 2}),
-    application_test_category: varchar('application_test_category', {length: 10}).notNull(),
-    application_vehicle_gearbox: mysqlEnum('application_vehicle_gearbox',
-                                           [
-                                             'Manual',
-                                             'Semi-Automatic',
-                                             'Automatic',
-                                           ]).notNull(),
-
-    previous_cancellation: json('previous_cancellation').$type<string[]>(),
-    business_id: int('business_id'),
-    business_name: varchar('business_name', {length: 255}),
-    business_address_line1: varchar('business_address_line1', {length: 255}),
-    business_address_line2: varchar('business_address_line2', {length: 255}),
-    business_address_line3: varchar('business_address_line3', {length: 255}),
-    business_address_line4: varchar('business_address_line4', {length: 255}),
-    business_address_line5: varchar('business_address_line5', {length: 255}),
-    business_postcode: varchar('business_postcode', {length: 20}),
-    business_telephone: varchar('business_telephone', {length: 50}),
-
-    examiner_visiting: tinyint('examiner_visiting'),
-  },
-);
+import { ScheduleBookings } from '@dvsa/mes-microservice-common/domain/scheduling-schema';
 
 /** Recursively removes nulls or undefined values from results to match the format generated by TARS' SQL statement*/
 const stripNulls = (value: any): any => {
@@ -229,6 +118,7 @@ export const getDSPTestSlots = async (
 
       testslotDurationMinutes: ScheduleBookings.testslot_duration_minutes,
       testslotStart: ScheduleBookings.testslot_start,
+      slotId: ScheduleBookings.slot_id,
       testcentreName: ScheduleBookings.testcentre_name,
       testcentreCostCode: ScheduleBookings.testcentre_cost_code,
       testslotVehicleTypeCode: ScheduleBookings.testslot_vehicle_type_code,
