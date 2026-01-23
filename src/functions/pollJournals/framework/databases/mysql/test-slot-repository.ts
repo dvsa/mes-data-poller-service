fffc4be0df8eafb25a97d94e6ea84079addb6859
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2026-01-23 11:38:56 +0000

    feat: add filter to mock data

--- src/functions/pollJournals/framework/databases/mysql/test-slot-repository.ts
+++ src/functions/pollJournals/framework/databases/mysql/test-slot-repository.ts
@@ -23,7 +23,7 @@ export const getTestSlots = async (
   testSlotRun?: number,
 ): Promise<ExaminerTestSlot[]> => {
   if (process.env.USE_MOCK_TARS_DATA) {
-    return TARSTestSlotMock(testSlotRun);
+    return TARSTestSlotMock(examinerIds, testSlotRun);
   }
   const sqlYearFormat = 'YYYY-MM-DD';
   const windowStart = moment(journalStartDate).format(sqlYearFormat);
