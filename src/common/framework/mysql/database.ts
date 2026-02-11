1de81637b92c422bfbf78445d882adedf4405f6e
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-02-11 14:10:01 +0000

    added clearer logs and error handling to my sql section of data-poller

--- src/common/framework/mysql/database.ts
+++ src/common/framework/mysql/database.ts
@@ -1,6 +1,7 @@
 import * as mysql from 'mysql2';
 import { config } from '../config/config';
 import { certificate } from '../../certs/ssl_profiles';
+import { error } from '@dvsa/mes-microservice-common/application/utils/logger';
 
 /**
  * Execute a single query and close connection
@@ -18,7 +19,7 @@ export const query = async (
     );
     queryResult = rows;
   } catch (err) {
-    console.log('err', err);
+    error('Error during query', err);
     throw err;
   } finally {
     connection.end();
@@ -31,19 +32,23 @@ export const query = async (
  * Establish a connection to a database to facilitate a single query
  */
 export const getTARSConnection = (): mysql.Connection => {
-  const configuration = config();
-  const connection = mysql.createConnection({
-    host: configuration.tarsReplicaDatabaseHostname,
-    database: configuration.tarsReplicaDatabaseName,
-    user: configuration.tarsReplicaDatabaseUsername,
-    password: configuration.tarsReplicaDatabasePassword,
-    charset: 'UTF8_GENERAL_CI',
-    ssl: process.env.TESTING_MODE ? null : certificate,
-    authPlugins: {
-      mysql_clear_password: () => () => Buffer.from(`${configuration.tarsReplicaDatabasePassword}\0`),
-    },
-  });
-  return connection;
+  try {
+    const configuration = config();
+    return mysql.createConnection({
+      host: configuration.tarsReplicaDatabaseHostname,
+      database: configuration.tarsReplicaDatabaseName,
+      user: configuration.tarsReplicaDatabaseUsername,
+      password: configuration.tarsReplicaDatabasePassword,
+      charset: 'UTF8_GENERAL_CI',
+      ssl: process.env.TESTING_MODE ? null : certificate,
+      authPlugins: {
+        mysql_clear_password: () => () => Buffer.from(`${configuration.tarsReplicaDatabasePassword}\0`),
+      },
+    });
+  } catch (err) {
+    error('During getTARSConnection', err);
+    throw err;
+  }
 };
 
 export const poolQuery = async (
@@ -57,45 +62,50 @@ export const poolQuery = async (
     );
     queryResult = rows;
   } catch (err) {
-    console.log('err', err);
+    error('Error during pool query', err);
     throw err;
   }
   return queryResult;
 };
 
 export const getConnectionPool = (connectionMode: 'TARS'|'DSP'): mysql.Pool => {
-  const configuration = config();
+  try {
+    const configuration = config();
 
-  let hostName: string = '';
-  let databaseName: string = '';
-  let databaseUserName: string = '';
-  let databasePassword: string = '';
+    let hostName: string = '';
+    let databaseName: string = '';
+    let databaseUserName: string = '';
+    let databasePassword: string = '';
 
-  switch (connectionMode) {
-  case 'TARS':
-    hostName = configuration.tarsReplicaDatabaseHostname;
-    databaseName = configuration.tarsReplicaDatabaseName;
-    databaseUserName = configuration.tarsReplicaDatabaseUsername;
-    databasePassword = configuration.tarsReplicaDatabasePassword;
-    break;
-  case 'DSP':
-    hostName = configuration.desDatabaseHostname;
-    databaseName = configuration.desDatabaseName;
-    databaseUserName = configuration.desDatabaseUsername;
-    databasePassword = configuration.desDatabasePassword;
-    break;
+    switch (connectionMode) {
+    case 'TARS':
+      hostName = configuration.tarsReplicaDatabaseHostname;
+      databaseName = configuration.tarsReplicaDatabaseName;
+      databaseUserName = configuration.tarsReplicaDatabaseUsername;
+      databasePassword = configuration.tarsReplicaDatabasePassword;
+      break;
+    case 'DSP':
+      hostName = configuration.desDatabaseHostname;
+      databaseName = configuration.desDatabaseName;
+      databaseUserName = configuration.desDatabaseUsername;
+      databasePassword = configuration.desDatabasePassword;
+      break;
+    }
+
+    return mysql.createPool({
+      host: hostName,
+      database: databaseName,
+      user: databaseUserName,
+      password: databasePassword,
+      charset: 'UTF8_GENERAL_CI',
+      ssl: process.env.TESTING_MODE ? null : certificate,
+      authPlugins: {
+        mysql_clear_password: () => () => Buffer.from(`${databasePassword}\0`),
+      },
+      connectionLimit: 50,
+    });
+  } catch (err) {
+    error('Error creating connection pool', err);
+    throw err;
   }
-
-  return mysql.createPool({
-    host: hostName,
-    database: databaseName,
-    user: databaseUserName,
-    password: databasePassword,
-    charset: 'UTF8_GENERAL_CI',
-    ssl: process.env.TESTING_MODE ? null : certificate,
-    authPlugins: {
-      mysql_clear_password: () => () => Buffer.from(`${databasePassword}\0`),
-    },
-    connectionLimit: 50,
-  });
 };
